3abfef72dfb5aa64ea34e916d8c017e9803655ed
João Daniel <jotaf.daniel@gmail.com>
2021-02-12 11:20:39 -0300

    extract routers
    Signed-off-by: João Daniel <jotaf.daniel@gmail.com>

---
+++ src/framework/routers/hackathon_router.ts
@@ -0,0 +1,22 @@
+import { Context } from 'koa';
+import * as Router from 'koa-router';
+
+import { createHackathon } from '../../use_cases/create_hackathon';
+import { HackathonService } from '../data_services/hackathon_service';
+
+export const hackathonRouter: Router = new Router();
+
+hackathonRouter.post(
+  '/hackathons',
+  async (ctx: Context): Promise<void> => {
+    try {
+      const ds = new HackathonService(ctx.db);
+      const newHackEvt = await createHackathon(ctx.request.body, ds);
+      ctx.broker.publish(newHackEvt);
+      ctx.body = { hackathon: newHackEvt.hackathon };
+    } catch (e) {
+      ctx.body = { error: e.message };
+      ctx.status = 403;
+    }
+  },
+);
